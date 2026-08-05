98309aa4321cd658d4eb065dfcb6a1af95c2050d
wzz-1230 <3135886265@qq.com>
2026-08-05 17:47:48 +0800

    Add 项目源代码/后端目录/middlewares/cors_middleware.py

---
+++ 项目源代码/后端目录/middlewares/cors_middleware.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+
+def add_cors_middleware(app: FastAPI) -> None:
+    """
+    添加跨域中间件
+
+    :param app: FastAPI对象
+    :return:
+    """
+    # 前端页面url
+    origins = ['*']
+    expose_headers = [
+        'x-body-encrypted',
+        'x-key-id',
+        'x-encrypt-alg',
+    ]
+
+    # 后台api允许跨域
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods=['*'],
+        allow_headers=['*'],
+        expose_headers=expose_headers,
+    )
